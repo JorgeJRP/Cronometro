26427514ce81753ce2227c660c1a7c91adacdda4
jorge rico <jj.rico.parser@gmail.com>
2021-01-03 18:51:28 +0100

    preescaler en counter

--- Cronometro.srcs/sources_1/new/top.vhd
+++ Cronometro.srcs/sources_1/new/top.vhd
@@ -2,13 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity top is
-    Port ( clk               : in  STD_LOGIC;
-           reset             : in  STD_LOGIC;
-           startstop         : in  STD_LOGIC;
-           up_down           : in STD_LOGIC;
+    Port ( 
+         clk               : in  STD_LOGIC;
+         reset             : in  STD_LOGIC;
+         startstop         : in  STD_LOGIC;
+         up_down           : in STD_LOGIC;
            
-           display_number    : out  STD_LOGIC_VECTOR (6 downto 0);
-           display_selection : out  STD_LOGIC_VECTOR (3 downto 0));
+         display_number    : out  STD_LOGIC_VECTOR (6 downto 0);
+         display_selection : out  STD_LOGIC_VECTOR (3 downto 0)
+         );
 end top;
 
 architecture Behavioral of top is
